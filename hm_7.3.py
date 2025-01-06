bdba6436ba1cccfb2d3cbb989665e0ba44cba9fd
YehorLitvinov <71943574+Weux94@users.noreply.github.com>
2025-01-06 12:33:31 +0200

    Update hm_7.3.py

--- hm_7.3.py
+++ hm_7.3.py
@@ -1,8 +1,3 @@
-from random import random
-
-sentence = 'Hello, hello'
-
-
 def second_index(text, letters):
     first_index = text.find(letters)
 
@@ -13,4 +8,8 @@ def second_index(text, letters):
 
     return second_index if second_index != -1 else None
 
+sentence = 'Hello, hello'
+sentence2 = 'Hello, lo lo lo'
+
 print(second_index(sentence, 'lo'))
+print(second_index(sentence2, 'lo'))
